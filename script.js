a85605d495bcbf0bf22730ff17fee58701d49b08
Γιάννης Καρασούλας <ioanniskarasoulas@gmail.com>
2024-04-18 20:27:45 +0300

    Add functions
    Added a functions to change the landing page 1 bio to latest project and vice versa

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function scrollToID(id) {
     document.getElementById(id).scrollIntoView();
 }
+
+var showBio = true;
+
+function showLatestProject() {
+
+    if (showBio) {
+        document.getElementById("bio").style.display = "none";
+        document.getElementById("latestproject").style.display = "block";
+    }
+    else if (!showBio) {
+        document.getElementById("bio").style.display = "block";
+        document.getElementById("latestproject").style.display = "none";
+    }
+
+    showBio = !showBio;
+}
