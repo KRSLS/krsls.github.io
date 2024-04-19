d07a49efa12f7994cdcbd56db8eace6d8895d680
Γιάννης Καρασούλας <ioanniskarasoulas@gmail.com>
2024-04-19 17:28:14 +0300

    Full push changes
    Added projects (WIP), changed classes and id's. Changes to css also and made a function to change projects, something like carousel. In the future this will be animated.
    
    This commit is big since there are parts that are connected to others.

--- script.js
+++ script.js
@@ -19,15 +19,63 @@ function showLatestProject() {
 }
 
 function sendEmail() {
-
     var subject = document.getElementById("emailSubject").value;
     var body = document.getElementById("emailBody").value;
 
     if (subject === '' || body === '') {
         alert('You need to enter a name & subject.')
     }
-    else if(subject != null && body != null){
+    else if (subject != null && body != null) {
         window.location = 'mailto:ioanniskarasoulas@gmail.com?subject=' + subject + '&body=' + body;
     }
+}
 
+var projectIndex = 0;
+var projects = [
+    p1 = document.getElementById("p1"),
+    p2 = document.getElementById("p2"),
+    p3 = document.getElementById("p3"),
+    p4 = document.getElementById("p4"),
+]
+
+function switchProject(where) {
+
+    if (where === 1) {
+        if (projectIndex < projects.length - 1) {
+            projectIndex++;
+        }
+        else projectIndex = 0;
+
+        console.log('Switched to project ' + projectIndex);
+
+        for (let index = 0; index < projects.length; index++) {
+            if(index === projectIndex)
+            {
+                projects[index].style.display = "block";
+            }
+            else if(index !== projectIndex)
+            {
+                projects[index].style.display = "none";
+            }
+        }
+    }
+    else if (where === -1){
+        if (projectIndex > 0) {
+            projectIndex--;
+        }
+        else projectIndex = projects.length - 1;
+
+        console.log('Switched to project ' + projectIndex);
+
+        for (let index = 0; index < projects.length; index++) {
+            if(index === projectIndex)
+            {
+                projects[index].style.display = "block";
+            }
+            else if(index !== projectIndex)
+            {
+                projects[index].style.display = "none";
+            }
+        }
+    }
 }
