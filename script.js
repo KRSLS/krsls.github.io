6c2c52445d96f8715fcc588fc2f4e570ba42fa96
Γιάννης Καρασούλας <ioanniskarasoulas@gmail.com>
2024-04-22 13:54:35 +0300

    Fixes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function scrollToID(id) {
     document.getElementById(id).scrollIntoView();
 }
+
+function navBarBorderOnScroll() {
+	if(document.getElementById("nav").style.top === '0px')
+	{
+		console.log('f');
+	}
+}
