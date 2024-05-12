aedbeee332c89a36afeb332663df1a3b14a0fa14
Γιάννης Καρασούλας <ioanniskarasoulas@gmail.com>
2024-05-12 23:29:32 +0300

    Adjust Dark Mode button
    Changes to max blur value and also made the animation quicker

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var scaleCoff = 0.8;
 
 // blur stuff
 var blurAmount = 0;
-var maxBlurAmount = 2;
+var maxBlurAmount = 1;
 var minBlurAmount = 0;
 var blurCoff = 0.001;
 var blurDecreaseCoff = 150;
